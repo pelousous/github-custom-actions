de6166e261c5ee5256a6c16f8018c5faf0490c94
davide <info@davideravasi.com>
2024-08-12 07:31:49 +0000

    feat: added secrets

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,19 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // 1) get some input values  to get the target of the deploy
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) upload files
+  const s3Uri = `s3://${bucket}`
+  // the exec command will automatically look
+  // for the secrets environments variables setted in deploy.yml
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice('hello from my custom Javascript Action!');
 }
 
